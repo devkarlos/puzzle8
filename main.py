ceae2b575e82f5d542812f497f97ad57f737c9bf
devkarlos <k.raffay@protonmail.com>
2024-03-22 21:56:25 +0100

    get_neighbours function working

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Board:
     def __init__(self, tiles):
-        self.tiles = tiles
-        self.size = 0
-        self.goal = [
+        self.tiles: list = tiles
+        self.size: int = 0
+        self.goal: list = [
             [1,2,3],
             [4,5,6],
             [7,8,0] # 0 represents the empty space
@@ -21,9 +21,7 @@ class Board:
         for i in range(0,3):
             if to_find in lst[i]:
                 return [i, lst[i].index(to_find)]
-            else:
-                continue
-        return None
+        return []
 
     # The Manhattan distance between a board and the goal board is the sum of the Manhattan distances
     # (sum of the vertical and horizontal distance) from the tiles to their goal positions. 
@@ -38,44 +36,97 @@ class Board:
                         distance += abs(goal_pos[p] - tile_pos[p])
         return distance
 
-    def isGoal(self) -> bool:
+    def is_goal(self) -> bool:
         return self.tiles == self.goal
     
     def equals(self, board) -> bool:
         return self.tiles == board
+    
+    def replace_item_in_tile(self, tile: list, item_pos: list, replace_with: int) -> list:
+        temp_tile: list = [row[:] for row in tile]
+        temp_tile[item_pos[0]][item_pos[1]] = replace_with
+        return temp_tile
+    
+    def switch_items_in_tile(self, tile: list, item_pos1: list, item_pos2: list) -> list:
+        temp_tile: list = [row[:] for row in tile]
+        temp_value = temp_tile[item_pos1[0]][item_pos1[1]]
+        
+        temp_tile[item_pos1[0]][item_pos1[1]] = temp_tile[item_pos2[0]][item_pos2[1]]
+        temp_tile[item_pos2[0]][item_pos2[1]] = temp_value
+        
+        return temp_tile
 
     # 0 in the corner -> 2 neigbors
     # 0 in the center -> 4 neigbors
     # 0 in the middle of row/column -> 3 neigbors
-    def neighbours(self) -> list:
-        neighbours = 0
+    def get_neighbours(self) -> list:
+        temp_tile: list = [row[:] for row in self.tiles]
+        neighbors: list = []
 
-        zero_pos = self.get_item_pos(self.tiles, 0)
-        zero_x = zero_pos[0]
-        zero_y = zero_pos[1]
+        zero_pos: list = self.get_item_pos(self.tiles, 0)
+        zero_x: int = zero_pos[0]
+        zero_y: int = zero_pos[1]
+        
+        # TODO: Recode this algorithm
+        if zero_x == 1 and zero_y in [0,2]: # zero is in the left/right center
+            neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x+1, zero_y])) # left/right center
+            neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x-1, zero_y])) # left/right center
+            if zero_y == 0:
+                neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x, zero_y-1])) # right top
+            else: # zero_y == 2
+                neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x, zero_y+1])) # left bottom
 
-        if zero_x == 1 and zero_y in [0,2]: # above and bellow the center
-            neighbours = 3
-        elif zero_x in [0,2] and zero_y == 1: # next to the center
-            neighbours = 3
+        elif zero_x in [0,2] and zero_y == 1: # zero is in the top/bottom center
+            neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x, zero_y+1])) # left/right center
+            neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x, zero_y-1])) # left/right center
+            if zero_x == 0:
+                neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x+1, zero_y])) # right top
+            else: # zero_x == 2
+                neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x-1, zero_y])) # left bottom
+
         elif zero_x in [0, 2] and zero_y in [0, 2]: # corners
-            neighbours = 2
-        else: # center
-            neighbours = 4
+            if zero_x == 0:
+                neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x, zero_y+1]))
+            else: # zero_x == 2
+                neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x, zero_y-1]))
+            if zero_y == 0:
+                neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x+1, zero_y]))
+            else: # zero_y == 2
+                neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x-1, zero_y]))
 
-        return neighbours
+        else: # center
+            neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x+1, zero_y]))
+            neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x-1, zero_y]))
+            neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x, zero_y+1]))
+            neighbors.append(self.switch_items_in_tile(temp_tile, zero_pos, [zero_x, zero_y-1]))
+
+        return neighbors
     
-    def isSolvable(self):
+    def is_solvable(self):
         pass
 
+    def print_tile(self):
+        for row in self.tiles:
+            print(row)
+
 tiles = [
-    [8,1,3],
-    [4,0,2],
+    [2,8,3],
+    [0,1,4],
     [7,6,5]
 ]
 
 board = Board(tiles)
 
-print(board.hamming())
-print(board.manhattan())
-print(board.neighbours())
+print("hamming:", board.hamming())
+print("manhattan:", board.manhattan())
+i = 0
+for neighbour in board.get_neighbours():
+    i+=1
+    print("neighbour:", i)
+    for row in neighbour:
+        print(row)
+    print("----")
+    
+print("\n----")
+print("neighbours:", i)
+board.print_tile()
